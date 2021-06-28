af7023b622a4cb37fe833d358b5f1277038a96af
rahat14 <rahatshovo6666@gmail.com>
2021-06-28 23:55:03 +0600

    updated

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\APIController;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// $router->group(['prefix' => 'api/v1'], function () use ($router) {
+
+
+     Route::get('/resturants' , 'App\Http\Controllers\APIController@AllResturant') ;
+
+
+     Route::get('/foods' , 'App\Http\Controllers\APIController@getFoodMenuViaResturant') ;
+
+
+
+
+    
