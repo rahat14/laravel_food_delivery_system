97f52c63d438960675586bd4818e7ca46d5cb824
iammashikur <700mirash@gmail.com>
2021-07-26 14:39:37 +0600

    Login methood added to DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+
+    public function login()
+    {
+    	return view('admin/' . 'login');
+    }
+
     public function index()
     {
     	return view('admin/' . 'index');
